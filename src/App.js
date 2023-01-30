db0f1befc29531f7cab66f0e9e187c2f27213f9a
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-01-30 11:26:01 -0300

    feat: Games selecionados armazenados no GamesSelectedContext

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Description from "./pages/Description/Description.js";
 import SignUp from "./components/SignUp/SignUp.js";
 import Login from "./components/Login/Login.js";
 import GamesProvider from "./components/contextAPI/GamesContext.js";
+import GamesSelectedProvider from "./components/contextAPI/GamesSelectedContext.js";
 
 
 function App() {
@@ -20,15 +21,17 @@ function App() {
 
       <Provider>
         <GamesProvider>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/success" element={<Success />} />
-            <Route path="/description/:id" element={<Description />} />
-          </Routes>
+          <GamesSelectedProvider>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/success" element={<Success />} />
+              <Route path="/description/:id" element={<Description />} />
+            </Routes>
+          </GamesSelectedProvider>
         </GamesProvider>
       </Provider>
     </BrowserRouter>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,26 +1,19 @@
 import { useContext, useState } from "react";
 import { GamesContext } from "../../components/contextAPI/GamesContext";
+import { GamesSelectedContext } from "../../components/contextAPI/GamesSelectedContext";
 import Game from "../../components/Game/Game";
 import Header from "../../components/Header /Header";
 import { getGames } from "../../services/rfrgames";
 import { StyledGames, StyledHomeContainer, StyledProducts } from "./style";
 
 export default function Home() {
-    const [selected, setSelected] = useState([]);
+    const { selected, selectItem } = useContext(GamesSelectedContext);
     const { games } = useContext(GamesContext);
-    console.log(games);
 
     const maisVendidos = games?.filter((g) => g.category === "M");
     const novidades = games?.filter((g) => g.category === "N");
     const emBreve = games?.filter((g) => g.category === "E");
 
-    function selectItem(objectGame) {
-        if (selected.includes(objectGame)) {
-            const removeSelection = selected.filter((g) => g != objectGame);
-            return setSelected(removeSelection);
-        }
-        setSelected([...selected, objectGame]);
-    }
 
     return (
         <>
